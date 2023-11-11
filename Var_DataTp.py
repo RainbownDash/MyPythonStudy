281a5a2dc5e85038db28fc13e30273bcd0de317f
Michael Dashko <mishaydashko2072@gmail.com>
2023-11-11 20:49:44 +0200

    Change some files

---
+++ Var_DataTp.py
@@ -0,0 +1,15 @@
+#import math
+def myFunc1():
+
+    n1 = int(input("Enter first num: "))
+    n2 = int(input("Enter first num: "))
+    n3 = int(input("Enter first num: "))
+    #Summa
+    suma = n1 + n2 + n3
+    mult = n1*n2*n3
+    power = n1 ** (n2-n3)
+    print(suma, mult, power)
+#myFunc1()
+
+n = input()
+
